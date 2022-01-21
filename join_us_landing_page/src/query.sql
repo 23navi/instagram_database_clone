44c0d5ea805796fb4b77bb6d6f34054d516594de
Navi sureka <navisureka23@gmail.com>
2022-01-21 18:44:11 +0530

    query add

--- join_us_landing_page/src/query.sql
+++ join_us_landing_page/src/query.sql
@@ -1,4 +1,13 @@
+# Create the mysql database and add users table in it.
+
+CREATE DATABASE join_us;
+USE join_us;
+
+
 CREATE TABLE users(
     email varchar(255) PRIMARY KEY not null,
     created_at timestamp DEFAULT now()
 );
+
+
+
